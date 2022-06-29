b77fff63ccb3827aeda9294de7ed2493e14c36ce
nicky-LV <nickystnkvs@outlook.com>
2022-06-29 17:36:27 +0100

    updated cascade rules

--- smtp_server/db.py
+++ smtp_server/db.py
@@ -1,8 +1,10 @@
+import datetime
 import os
 from sqlalchemy.orm import declarative_base, relationship, Session
-from sqlalchemy import Column, String, Integer, ForeignKey, Boolean
+from sqlalchemy import Column, String, Integer, ForeignKey, Boolean, DateTime, delete
 from sqlalchemy.engine import create_engine
-from sqlalchemy.dialects.postgresql import UUID, ARRAY
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import event
 
 if int(os.environ['TEST_DB']) == 0:
     engine = create_engine(f"postgresql://{os.environ['POSTGRES_USERNAME']}:{os.environ['POSTGRES_PASSWORD']}@{os.environ['DB_HOST']}:{os.environ['DB_PORT']}",
@@ -26,7 +28,7 @@ class Users(Base):
     email_address = Column(String, nullable=False, unique=True)
 
     # M2M relationship to emails table
-    emails = relationship('Emails', secondary='user_emails', back_populates='users', cascade="all, delete")
+    emails = relationship('Emails', secondary='user_emails', back_populates='users')
 
     def __repr__(self):
         return f"User {self.uuid}: {self.email_address}"
@@ -40,6 +42,7 @@ class Emails(Base):
     sender = Column(String(320), nullable=False)
     body = Column(String, nullable=True)
     retrieved = Column(Boolean, nullable=False, default=False)
+    datetime = Column(DateTime, nullable=False, default=datetime.datetime.utcnow())
 
     users = relationship('Users', secondary='user_emails', back_populates='emails')
 
@@ -51,12 +54,10 @@ class UserEmails(Base):
     __tablename__ = 'user_emails'
 
     id = Column(Integer, primary_key=True)
-    user_uuid = Column(String, ForeignKey('users.uuid', ondelete="CASCADE"))
-    emails_id = Column(Integer, ForeignKey('emails.id', ondelete="CASCADE"))
+    user_uuid = Column(String, ForeignKey('users.uuid'))
+    emails_id = Column(Integer, ForeignKey('emails.id'))
 
 
 # Create tables if they don't exist
 if not Base.metadata.tables and not os.environ['TEST_DB']:
     Base.metadata.create_all(engine)
-
-
